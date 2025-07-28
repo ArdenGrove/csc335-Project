04f47d8f24e7d67fb2e6da9e179c24ece3ac157a
ArdenGrove <ardenbondgrove1@gmail.com>
2025-07-28 17:29:27 +1200

    Add files via upload

--- Simulation.java
+++ Simulation.java
@@ -8,27 +8,20 @@
 public class Simulation
 {
     UserInputReceiver UI = new UserInputReceiver();
-    Person VL = new Person();
-    
-    
-    public Simulation()
-    {
-        //code for the movement of persons 
-        //direction for up(0), down(1), left(2), and right(3). represented by a number from 0 to 3
-        //the X and Y axis start at the bottem left corrner with Y being vertical and X being horisontal
-        
-        if (VL.direction == 0){
-            while (VL.y <= UI.inputedSpaceLength){
-            }
-            }else if(VL.direction == 1){
-        
-        
-            }else if(VL.direction == 2){
-         
-        
-            }else if(VL.direction == 3){
 
-  
+    //direction for up(0), down(1), left(2), and right(3). represented by a number from 0 to 3
+    //the X and Y axis start at the bottem left corrner with Y being vertical and X being horisontal    
+    public Simulation(UserInputReceiver UI, People population) {
+        for (Person movement : population.everyBody) {
+            if (movement.direction == 0 && movement.y < UI.inputedSpaceLength) {
+                movement.y++;
+            } else if (movement.direction == 1 && movement.y > 0) {
+                movement.y--;
+            } else if (movement.direction == 2 && movement.x > 0) {
+                movement.x--;
+            } else if (movement.direction == 3 && movement.x < UI.inputedSpaceWidth) {
+                movement.x++;
+            }
         }
-    }
+    }   
 }
